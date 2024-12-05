2971b7d3b530a69792c38d7262f262f93d8a4534
Kirill Salnikov <kirill18191@yandex.ru>
2024-12-05 17:49:10 +0300

    fixes

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-from rest_framework_simplejwt import views as jwt_views
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
 app_name = 'api'
